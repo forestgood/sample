36c34ae8ec9cf55110fb936cea5646aede8f0b64
Forest Good <fgoodiu@gmail.com>
2013-11-04 10:31:02 -0500

    update contact info

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Sample::Application.routes.draw do
   resources :microposts
   root to: 'static_pages#home'
 
-  #match 'signup', to: 'users#new'
+  match 'signup', to: 'users#new'
   match 'signin', to: 'sessions#new'
   match 'signout', to: 'sessions#destroy', via: 'delete'
 
